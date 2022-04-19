30de914a874cd756904f6dc94e181766e052bbd6
moskbohd <moskbohd@gmail.com>
2022-04-19 11:36:29 +0300

    Start

---
+++ src/com/MoskBohd/IsoscelesTrapezoid.java
@@ -0,0 +1,78 @@
+package com.MoskBohd;
+
+import java.util.Objects;
+
+public class IsoscelesTrapezoid {
+    double lenghtUp = 1;
+    double lenghtDown = 1.5;
+    double height = 1;
+    double area = height*(lenghtUp+lenghtDown)/2;
+
+    // Constructor
+    public IsoscelesTrapezoid(int lenghtUp, int lenghtDown, int height) {
+        this.lenghtUp = lenghtUp;
+        this.lenghtDown = lenghtDown;
+        this.height = height;
+    }
+
+    // Getters/Setters
+    public double getLenghtUp() {
+        return lenghtUp;
+    }
+
+    public void setLenghtUp(double lenghtUp) {
+        this.lenghtUp = lenghtUp;
+    }
+
+    public double getLenghtDown() {
+        return lenghtDown;
+    }
+
+    public void setLenghtDown(double lenghtDown) {
+        this.lenghtDown = lenghtDown;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    // 5 methods
+
+    
+    // Override toString() method
+    @Override
+    public String toString() {
+        return "IsoscelesTrapezoid{" +
+                "lenghtUp=" + lenghtUp +
+                ", lenghtDown=" + lenghtDown +
+                ", height=" + height +
+                ", area=" + area +
+                '}';
+    }
+
+    // Override hash() and equals() methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IsoscelesTrapezoid that = (IsoscelesTrapezoid) o;
+        return Double.compare(that.getLenghtUp(), getLenghtUp()) == 0 && Double.compare(that.getLenghtDown(), getLenghtDown()) == 0 && Double.compare(that.getHeight(), getHeight()) == 0 && Double.compare(that.getArea(), getArea()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLenghtUp(), getLenghtDown(), getHeight(), getArea());
+    }
+}
---
+++ src/com/MoskBohd/Main.java
@@ -0,0 +1,13 @@
+package com.MoskBohd;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        IsoscelesTrapezoid isoscelesTrapezoid = new IsoscelesTrapezoid(2, 4, 3);
+        isoscelesTrapezoid.setHeight(2);
+        System.out.println(isoscelesTrapezoid.getArea());
+
+
+    }
+}
