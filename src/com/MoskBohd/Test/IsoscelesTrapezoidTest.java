b55673ef2157ec136af84cac502542e6fa06d053
moskbohd <moskbohd@gmail.com>
2022-08-05 09:49:33 +0200

    added package Test

--- src/com/MoskBohd/Test/IsoscelesTrapezoidTest.java
+++ src/com/MoskBohd/Test/IsoscelesTrapezoidTest.java
@@ -1,5 +1,6 @@
-package com.MoskBohd;
+package com.MoskBohd.Test;
 
+import com.MoskBohd.IsoscelesTrapezoid;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
